d7cb24d6de8303d09d4b85d0ac8d1e002be6d005
Luis Paulo Lessa <prof.lpjunior@gmail.com>
2026-08-26 12:53:32 -0300

    test: ajuste no log de variaveis de ambiente

--- config/settings.py
+++ config/settings.py
@@ -14,12 +14,12 @@ DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
-print("=== ENV DEBUG ===")
+print("--- ENV DEBUG ---")
 print("VERCEL:", repr(os.getenv("VERCEL")))
 print("VERCEL_ENV:", repr(os.getenv("VERCEL_ENV")))
 print("ALLOWED_HOSTS existe:", "ALLOWED_HOSTS" in os.environ)
 print("ALLOWED_HOSTS:", repr(os.getenv("ALLOWED_HOSTS")))
-print("=================")
+print("------------------------")
 
 # Application definition
 
